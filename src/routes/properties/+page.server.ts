8b8a422a18cc6528fb217db5f9878a54e72d0b54
sch-28 <jansch2803@googlemail.com>
2022-08-22 02:35:02 +0200

    basic filter logic implemented

--- src/routes/properties/+page.server.ts
+++ src/routes/properties/+page.server.ts
@@ -1,6 +1,6 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad, Action } from './$types';
-import { Search_Query } from './search_query';
+import { api_request, Search_Query } from './api';
 
 export const load: PageServerLoad = async (load_event) => {
 	let params = load_event.url.searchParams;
@@ -19,18 +19,8 @@ export const load: PageServerLoad = async (load_event) => {
 			console.log('invalid param');
 		}
 	}
-	/* const result = await fetch(
-		`https://webapi.resales-online.com/V6/SearchProperties?p_agency_filterid=1&p1=1027300&p2=f78c58fe0bddd3d516350a5060bbe45271d7f9d2&P_output=JSON&P_all=True&P_sandbox=true`,
-		{
-			method: 'GET',
-			headers: {
-				'content-type': 'application/json'
-			}
-		}
-	); */
-	//const result_json = await result.json();
-	//console.log(result_json);
-	//
 
-	return {  params:search_query  };
+	const result = await api_request(search_query);
+
+	return { params: search_query, results: result };
 };
---
+++ src/routes/properties/api.ts
@@ -0,0 +1,110 @@
+import { VITE_API_KEY, VITE_API_IDENTIFIER } from '$env/static/private';
+
+export class Search_Query {
+	purchase_type: 'buy' | 'rent' | 'dev';
+	location: string;
+	min_price: number;
+	max_price: number;
+	min_bedrooms: number;
+	property_type: number;
+
+	constructor() {
+		this.purchase_type = 'buy';
+		this.location = '';
+		this.min_price = 0;
+		this.max_price = 0;
+		this.min_bedrooms = 0;
+		this.property_type = 0;
+	}
+
+	get purchase_type_int() {
+		switch (this.purchase_type) {
+			case 'buy':
+				return 1;
+			case 'rent':
+				return 2;
+		}
+	}
+}
+export interface PropertyType {
+	NameType: string;
+	Type: string;
+	TypeId: string;
+	Subtype1: string;
+	SubtypeId1: string;
+}
+
+export interface Status {
+	system: string;
+	en: string;
+}
+
+export interface Property {
+	Reference: string;
+	AgencyRef: string;
+	Country: string;
+	Province: string;
+	Area: string;
+	Location: string;
+	SubLocation: string;
+	PropertyType: PropertyType;
+	Status: Status;
+	Bedrooms: string;
+	Bathrooms: string;
+	Currency: string;
+	Price: string;
+	OriginalPrice: number;
+	Dimensions: string;
+	Built: number;
+	Terrace: number;
+	GardenPlot: number;
+	CO2Rated: string;
+	EnergyRated: string;
+	OwnProperty: string;
+	Pool: number;
+	Parking: number;
+	Garden: number;
+	Description: string;
+}
+
+export interface QueryInfo {
+	ApiId: string;
+	QueryId: string;
+	SearchType: string;
+	PropertyCount: number;
+	CurrentPage: number;
+	PropertiesPerPage: number;
+}
+
+export interface Success_Response {
+	transaction: {
+		status: 'success';
+		errordescription: undefined;
+	};
+	QueryInfo: QueryInfo;
+	Property: Property[];
+}
+export interface Error_Response {
+	transaction: {
+		status: 'error';
+		errordescription: string;
+	};
+	QueryInfo: undefined;
+	Property: undefined;
+}
+
+export type Response = Success_Response | Error_Response;
+
+export async function api_request(search_query: Search_Query) {
+	console.log(VITE_API_KEY);
+	const request_options = {
+		method: 'GET'
+	};
+
+	const request_url = `https://webapi.resales-online.com/V6/SearchProperties?p_agency_filterid=${search_query.purchase_type_int}&p1=${VITE_API_IDENTIFIER}&p2=${VITE_API_KEY}&p_beds=${search_query.min_bedrooms}x&p_min=${search_query.min_price}&p_max=${search_query.max_price}&p_location=${search_query.location}&P_output=JSON&P_all=True`;
+
+	const result = await fetch(request_url, request_options);
+	const result_json = (await result.json()) as Response;
+
+	return result_json;
+}
