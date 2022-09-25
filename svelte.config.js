58bccf5ce7356fa8f9dedf0872300fe9bdb2486c
sch-28 <jansch2803@googlemail.com>
2022-09-25 15:10:23 +0200

    Added bulma

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,17 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 import { optimizeImports } from "carbon-preprocess-svelte";
+import postcss from 'rollup-plugin-postcss';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(optimizeImports()),
-
+	preprocess: preprocess(optimizeImports(), postcss()),
 	kit: {
 		adapter: adapter()
 	}
+	
 };
 
 export default config;
