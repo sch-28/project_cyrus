303d38a3bcfc454cf89a22d1ba4611151f48b216
sch-28 <jansch2803@googlemail.com>
2022-09-30 13:39:27 +0200

    Fixed Type error

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,5 @@
+declare module 'simple-svelte-autocomplete';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
@@ -6,13 +8,4 @@ declare namespace App {
 	// interface Platform {}
 	// interface PrivateEnv {}
 	// interface PublicEnv {}
-}
-import type { SvelteComponentTyped } from "svelte";
-
-declare module 'simple-svelte-autocomplete' {
-	export default class AutoComplete extends SvelteComponentTyped<{propertyName: string;}> {
-		$$prop_def: any;
-		$$events_def: any;
-		$$slot_def: any;
-	}
 }
